596b2e744bc0c290f4982c18fff96b25939f4070
Doma-0609 <amir.nahviii@gmail.com>
2024-12-04 20:42:46 +0330

    Update w.lua

--- w.lua
+++ w.lua
@@ -1,91 +1,101 @@
-script_key = "QXoQVLGnhMFeSDHVgOfUGzjYPDhHwwrc"
-getgenv().Team = "Pirates"
-getgenv().Setting = { -- Select Weapon, Self Explain
+--[[
+	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
+]]
+
+getgenv().config = {
+    ["Team"] = "Pirates",
+    ["Use Race"] = {
+        ["V3"] = true,
+        ["V4"] = true
+    },
+    ["Info Screen"] = true,
+    ["White Screen"] = false,
+    ["BypassTp"] = true,
+    ["SkipFruit"] = {
+        "Portal-Portal"
+    },
+    ["Skip Race V4 User"] = true,
+    ["MainSkillToggle"] = {
         ["Melee"] = {
             ["Enable"] = true,
-            ["Delay"] = 3,
+            ["Delay"] = 1.75,
             ["Skills"] = {
                 ["Z"] = {
                     ["Enable"] = true,
-                    ["HoldTime"] = 0,
-                    ["TimeToNextSkill"] = 0,
+                    ["HoldTime"] = 1.5,
                 },
-            [ "X"] = {
+                [ "X"] = {
                     ["Enable"] = true,
                     ["HoldTime"] = 0,
-                    ["TimeToNextSkill"] = 0,
                 },
-    
+
                 ["C"] = {
                     ["Enable"] = true,
                     ["HoldTime"] = 0,
-                    ["TimeToNextSkill"] = 0,
                 },
             },
         },
         ["Blox Fruit"] = {
-            ["Enable"] = false,
-            ["Delay"] = 4,
-            ["Skills"] = {
-                ["Z"] = {
-                    ["Enable"] = true,
-                    ["HoldTime"] = 0,
-                    ["TimeToNextSkill"] = 0,
-                },
-                ["X"] = {
-                    ["Enable"] = true,
-                    ["HoldTime"] = 0,
-                    ["TimeToNextSkill"] = 0,
-                },
-    
-                ["C"] = {
-                    ["Enable"] = true,
-                    ["HoldTime"] = 0,
-                    ["TimeToNextSkill"] = 0,
-                },
-                ["V"] = {
-                    ["Enable"] = false,
-                    ["HoldTime"] = 0,
-                    ["TimeToNextSkill"] = 0,
-                },
-                ["F"] = {
-                    ["Enable"] = false,
-                    ["HoldTime"] = 0,
-                    ["TimeToNextSkill"] = 0,
-                },
-            },
-        },
-        ["Sword"] = {
-            ["Enable"] = false,
+            ["Enable"] = true,
             ["Delay"] = 2,
             ["Skills"] = {
                 ["Z"] = {
                     ["Enable"] = true,
-                    ["HoldTime"] = 1,
-                    ["TimeToNextSkill"] = 0,
+                    ["HoldTime"] = 0,
                 },
                 ["X"] = {
                     ["Enable"] = true,
+                    ["HoldTime"] = 2,
+                },
+                ["C"] = {
+                    ["Enable"] = true,
                     ["HoldTime"] = 0,
-                    ["TimeToNextSkill"] = 0,
+                },
+                ["V"] = {
+                    ["Enable"] = false,
+                    ["HoldTime"] = 0,
+                },
+                ["F"] = {
+                    ["Enable"] = true,
+                    ["HoldTime"] = 0,
                 },
             },
         },
         ["Gun"] = {
             ["Enable"] = true,
-            ["Delay"] = 2,
+            ["Delay"] = 1,
             ["Skills"] = {
                 ["Z"] = {
                     ["Enable"] = true,
                     ["HoldTime"] = 0,
-                    ["TimeToNextSkill"] = 0,
                 },
                 ["X"] = {
                     ["Enable"] = true,
                     ["HoldTime"] = 0,
-                    ["TimeToNextSkill"] = 0,
+                },
+            },
+        },
+        ["Sword"] = {
+            ["Enable"] = true,
+            ["Delay"] = 1,
+            ["Skills"] = {
+                ["Z"] = {
+                    ["Enable"] = true,
+                    ["HoldTime"] = 0,
+                },
+                ["X"] = {
+                    ["Enable"] = true,
+                    ["HoldTime"] = 0,
                 },
             },
         }
-    }
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/7c85f07250e76490265b724cca4e1d9c.lua"))()
+    },
+    ["Webhooks"] = {
+        ["Link Webhook"] = "",
+        ["Toggle Webhook"] = true
+    },
+    ["MinBountyHunt"] = 0,
+    ["MaxBountyHunt"] = 30000000,
+    ["SafeHealth"] = 4000
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LumosSera/SeraHub/main/AutoBounty.lua"))()
