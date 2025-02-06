e75500a9d0061b5ba1d354a40651c0085dcbf0e6
Doma-0609 <amir.nahviii@gmail.com>
2025-02-06 15:34:02 +0330

    Update a.lua

--- a.lua
+++ a.lua
@@ -1,1 +1,6 @@
-ksnnt
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))()
+-- This script should be placed in a ServerScript or LocalScript
+wait(4)
+local Players = game:GetService("Players")
+game.Players.LocalPlayer:Kick("You have been permanently banned.")
