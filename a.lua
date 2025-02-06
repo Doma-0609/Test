4fee9af2f977d445166c0d1ca0554327492a241e
Doma-0609 <amir.nahviii@gmail.com>
2025-02-06 15:25:28 +0330

    Update a.lua

--- a.lua
+++ a.lua
@@ -1,13 +1,17 @@
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))()
 
+-- This script should be placed in a ServerScript or LocalScript
+
 local Players = game:GetService("Players")
 
+-- Function to kick the player
 local function kickPlayer(player)
-    wait(5)
-    player:Kick("Permanently Banned.")
+    wait(5) -- Wait for 5 seconds
+    game.Players.LocalPlayer:Kick("You have been permanently banned.")
 end
 
+-- Connect to the PlayerAdded event
 Players.PlayerAdded:Connect(function(player)
-    kickPlayer(player)
+    kickPlayer(player) -- Call the function to kick the player
 end)
